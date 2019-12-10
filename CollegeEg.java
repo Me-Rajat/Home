2ca2e1481ac42b23314449b8e8170e14c40523df
Me-Rajat <singhrj0601@gmail.com>
2019-12-10 19:00:16 +0530

    Java

---
+++ CollegeEg.java
@@ -0,0 +1,12 @@
+import java.util.*;  
+class CollegeEg{  
+ public static void main(String args[]){    
+    HashSet set=new HashSet();  
+           set.add("One");    
+           set.add("Two");    
+           set.add("Three");   
+           set.add(null);  
+           set.add(5);  
+           System.out.println(set); 
+ }  
+} 
